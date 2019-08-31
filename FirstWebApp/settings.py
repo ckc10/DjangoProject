9163004498c3296306f6f91657e0b836b57f9c7b
ckc10 <ckchaudhari10@gmail.com>
2019-08-31 10:45:42 +0530

    Added django-heroku

--- FirstWebApp/settings.py
+++ FirstWebApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
